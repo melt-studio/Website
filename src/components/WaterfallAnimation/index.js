cfd457b6df8840c98712959eae329e3d84be440a
git1029 <gidesign+github@pm.me>
2023-03-30 17:37:28 +0100

    Use device height metric to center canvas on mobile

--- src/components/WaterfallAnimation/index.js
+++ src/components/WaterfallAnimation/index.js
@@ -1,60 +1,61 @@
-import { useEffect, useState } from 'react'
-import { Canvas } from '@react-three/fiber'
-import LevaControls from '../helpers/LevaControls'
-import PerfMonitor from '../helpers/PerfMonitor'
-import Scene from './Scene'
+import { useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import LevaControls from "../helpers/LevaControls";
+import PerfMonitor from "../helpers/PerfMonitor";
+import Scene from "./Scene";
 
-import { useConfig } from '../helpers/LevaControls/setupConfig'
-import { useToggleControls } from '../helpers/toggleControls'
-import { getLocalStorageConfig } from '../helpers/LevaControls/setupConfig'
-import defaultConfig from '../helpers/LevaControls/config.json'
-import { useRef } from 'react'
+import { useConfig } from "../helpers/LevaControls/setupConfig";
+import { useToggleControls } from "../helpers/toggleControls";
+import { getLocalStorageConfig } from "../helpers/LevaControls/setupConfig";
+import defaultConfig from "../helpers/LevaControls/config.json";
+import { useRef } from "react";
 
 const glSettings = {
   antialias: false,
-}
+};
 
 const created = ({ gl }) => {
-  gl.domElement.id = 'waterfallAnimation'
-  gl.setClearAlpha(0)
-}
+  gl.domElement.id = "waterfallAnimation";
+  gl.setClearAlpha(0);
+};
 
 const WaterfallAnimation = ({ controls, mobile }) => {
-  const { metric, value } = defaultConfig.devices.mobile
+  const { metric, value } = defaultConfig.devices.mobile;
   const [name, setName] = useState(
     (mobile && controls) ||
       window[`inner${metric[0].toUpperCase() + metric.slice(1)}`] < value
-      ? 'waterfall-mobile'
-      : 'waterfall'
-  )
+      ? "waterfall-mobile"
+      : "waterfall"
+  );
   const updateName = (newName) => {
-    setName(newName)
-  }
+    setName(newName);
+  };
 
   useEffect(() => {
     if (controls) {
-      document.body.classList.add('controls')
+      document.body.classList.add("controls");
     } else {
-      document.body.classList.remove('controls')
+      document.body.classList.remove("controls");
     }
-  }, [controls])
+  }, [controls]);
 
-  const [config, updateConfig] = useConfig(name)
-  const localStorageConfig = getLocalStorageConfig(name)
-  useToggleControls(controls === undefined ? false : controls)
+  const [config, updateConfig] = useConfig(name);
+  const localStorageConfig = getLocalStorageConfig(name);
+  useToggleControls(controls === undefined ? false : controls);
 
-  const container = useRef()
+  const container = useRef();
 
   return (
     <>
       <LevaControls controls={controls === undefined ? false : controls} />
       <div
         ref={container}
+        className="canvas-container"
         style={{
-          width: mobile && controls ? '390px' : '100%',
-          height: mobile && controls ? '844px' : '100vh',
-          display: 'block',
-          position: 'fixed',
+          width: mobile && controls ? "390px" : "100%",
+          height: mobile && controls ? "844px" : "100dvh",
+          display: "block",
+          position: "fixed",
           top: 0,
         }}
       >
@@ -72,7 +73,7 @@ const WaterfallAnimation = ({ controls, mobile }) => {
         </Canvas>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default WaterfallAnimation
+export default WaterfallAnimation;
