44361b92e8fc15da07492cea4b100751730de01f
git1029 <gidesign+github@pm.me>
2025-05-20 23:24:17 +0100

    Add document page

---
+++ src/services/documents.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const baseUrl = "/.netlify/functions/documents";
+
+const getDocument = async (pageUrl: string) => {
+  const response = await axios.put(baseUrl, { pageUrl });
+  return response.data;
+};
+
+const unlockDocument = async (id: string, password: string) => {
+  const response = await axios.put(baseUrl, { id, password });
+  return response.data;
+};
+
+const documentService = { getDocument, unlockDocument };
+
+export default documentService;
