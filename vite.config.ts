f7d3ce08d452e0d97bb5a0babf5992586a308c85
git1029 <gidesign+github@pm.me>
2025-09-12 19:41:17 +0100

    Update packages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  assetsInclude: ["**/*.obj"],
+  server: {
+    proxy: {
+      "/.netlify/functions/": "http://localhost:9999/",
+    },
+  },
   plugins: [react(), tailwindcss()],
 });
